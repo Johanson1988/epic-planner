cb06ec17d3b7b61093766fea8f7d5f5f70c51e50
nurlopez <nlopezcolom@gmail.com>
2019-11-21 19:59:19 +0100

    Fill app.js

---
+++ app.js
@@ -0,0 +1,60 @@
+'use strict'
+
+require('dotenv').config();
+
+const bodyParser   = require('body-parser');
+const cookieParser = require('cookie-parser');
+const express      = require('express');
+const hbs          = require('hbs');
+const mongoose     = require('mongoose');
+const logger       = require('morgan');
+const path         = require('path');
+
+const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
+
+const dbName = 'epic-planner-db';
+const dbUrl = 'mongodb://localhost:27017/';
+
+
+mongoose
+    .connect(dbUrl + dbName, {useNewUrlParser: true, useUnifiedTopology:true})
+        .then( mongoEntry => {
+            console.log(`Connected to Mongo! Database name: "${mongoEntry.connections[0].name}"`)
+        })
+        .catch(err => {
+            console.error('Error connecting to mongo', err)
+        });
+
+const app = express (); 
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(cookieParser());
+
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+app.use(express.static(path.join(__dirname, 'public')));
+// add favicon insert here <<<---
+
+app.locals.title = 'Epic-planner';
+
+app.use(
+        session({                                   //// cookie is a saved session
+            secret: process.env.SESSION_SECRET,  
+            resave: true,
+            saveUninitialized: false,
+            store: new MongoStore ({ 
+                mongooseConnection: mongoose.connection, 
+                ttl: 60 * 60 * 24 
+            }),
+        }),
+);
+
+const index = require('./routes/index');
+app.use('/', index);
+
+module.exports = app;
+
