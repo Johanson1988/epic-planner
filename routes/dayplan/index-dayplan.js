7ca078cc36770eb31a87d9dc5aa067091920f225
Johann <johanson88@gmail.com>
2019-11-22 18:37:25 +0100

    Route select-data OK

--- routes/dayplan/index-dayplan.js
+++ routes/dayplan/index-dayplan.js
@@ -2,4 +2,8 @@ const express = require('express');
 const router = express.Router();
 
 
+router.get('/select-date', (req,res,next) => {
+    res.render('./dayplan/select-date');
+})
+
 module.exports = router;
