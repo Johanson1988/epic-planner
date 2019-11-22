8ec8f3c22af452a1e9673d2b6128c4ed4162220c
nurlopez <nlopezcolom@gmail.com>
2019-11-22 15:56:00 +0100

    User.create OK

--- models/User.js
+++ models/User.js
@@ -2,7 +2,9 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema; 
 
 const userSchema = new Schema({
-    username: { type: String, required: true, unique: true }, // username = email with @ restriction
+    
+    fullName: { type: String},
+    email: { type: String, required: true, unique: true }, // username = email with @ restriction
     password: { type: String, required: true },
     location: { type: String, enum: ["Barceloneta", "Forum", "Gracia", "Horta-Guinardó", "Les Corts", "Poble Nou","Port Olimpic", "Port Vell", "Sant Andreu", "Sants", "Sant Martí", "Sarrià-Tibidabo" ] },
     keywords: { type: [] },
--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const zxcvbn = require('zxcvbn');
+const User = require('./../models/User');
 
 
 // bcrypt
@@ -12,7 +13,6 @@ const saltRounds = 10;
 router.post('/signup', (req, res, next) => {
     
     const {fullName, email, password, location } = req.body;
-    console.log(req.body);
     
     if (email === '' || password ==='') {
         res.render('./signin', {errorMessage:"Username or password cannot be empty."}); // insert here
@@ -22,9 +22,23 @@ router.post('/signup', (req, res, next) => {
         res.render('./signin', {errorMessage: "The Password is too weak, please try again"}); // insert here
         return;
       }
-    // TODO Postman post request
     
-})
+      User.findOne( {email} )
+            .then (emailFromDb => {
+                if ( emailFromDb) {
+                    res.render('./signin', {errorMessage: "email already exists"}); // <<<--- insert here)
+                    return; 
+                }
+                const salt = bcrypt.genSaltSync(saltRounds);
+                const hashedPassword = bcrypt.hashSync(password, salt);
+                User.create({fullName, email, password: hashedPassword, location})
+                    .then(() => res.redirect('/')) // TODO check route
+                    .catch(() => res.render ('./signin', {errorMessage:"An error while creating User"}) /// <<<--- insert here)
+                    );
+            })
+            .catch((err) => console.error(err));
+});
+
 
 
 
