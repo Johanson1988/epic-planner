8de076c6edf9e4ad723825845528aa241858e2d6
Johann <johanson88@gmail.com>
2019-11-23 18:14:46 +0100

    Update ref from dayplan to agenda user model create newdayplan OK add newdayplan to user.agenda OK add not-found view

--- routes/dayplan/edit-dayplan.js
+++ routes/dayplan/edit-dayplan.js
@@ -16,10 +16,28 @@ router.post('/', (req,res,next) => {
         events:[],
     });
     console.log('session',req.session.currentUser);
-    User.updateOne({_id})
-        .then((user) => {
+    newDayPlan.save(err=> {
+        if (err) {
+            console.error(err);
+            res.render('dayplan/edit-dayplan',{errorMessage:'ERROR'}); //TODO Insert here <---------            
+        }else {
+            console.log('Added New Dayplan to the dataBase');
+            const userId = req.session.currentUser._id;
+            console.log('userId', userId);
+            /* db.students.update(
+   { _id: 1 },
+   { $push: { scores: 89 } }
+)*/
+            User.updateOne({_id:userId}, {$push: {agenda: newDayPlan._id}})
+                .then(() => {
+                    console.log('Day added to agenda');
+
+                })
+                .catch((err) => console.error(err));
             
-        })
+    }
+    });
+    
 });
 
 module.exports = router;
