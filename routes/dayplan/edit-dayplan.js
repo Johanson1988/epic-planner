c424b95c0adf16660660b1468932054dc5120f80
Johann <johanson88@gmail.com>
2019-11-23 18:26:09 +0100

    update render to select date edit-dayplan Add errorhandler  select-date.hbs

--- routes/dayplan/edit-dayplan.js
+++ routes/dayplan/edit-dayplan.js
@@ -8,8 +8,7 @@ router.post('/', (req,res,next) => {
     console.log(req.body);
     const {'day-plan-date': selectedDate} = req.body;
     console.log(selectedDate);
-
-    res.render('./dayplan/edit-dayplan',{selectedDate});
+    
     const newDayPlan = new DayPlan({
         name:'null',
         date:selectedDate,
@@ -19,18 +18,16 @@ router.post('/', (req,res,next) => {
     newDayPlan.save(err=> {
         if (err) {
             console.error(err);
-            res.render('dayplan/edit-dayplan',{errorMessage:'ERROR'}); //TODO Insert here <---------            
+            res.render('dayplan/select-date',{errorMessage:err}); //TODO Insert here <---------            
         }else {
             console.log('Added New Dayplan to the dataBase');
             const userId = req.session.currentUser._id;
             console.log('userId', userId);
-            /* db.students.update(
-   { _id: 1 },
-   { $push: { scores: 89 } }
-)*/
+ 
             User.updateOne({_id:userId}, {$push: {agenda: newDayPlan._id}})
                 .then(() => {
                     console.log('Day added to agenda');
+                    res.render('./dayplan/edit-dayplan',{selectedDate});
 
                 })
                 .catch((err) => console.error(err));
