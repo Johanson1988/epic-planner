f44b5b170a71aacad198f438016950b849a33a32
Johann <johanson88@gmail.com>
2019-11-24 20:25:10 +0100

    Add events to specific dayplan in User OK

--- routes/dayplan/save-event.js
+++ routes/dayplan/save-event.js
@@ -9,8 +9,15 @@ const dbUrl = 'mongodb://localhost:27017/';
 const DayPlan = require('./../../models/Dayplan');
 const User = require('../../models/User');
 
-router.post('/', (req, res, next) =>{
-    
+router.post('/', (req, res, next) => {
+    const {eventId, dayPlanId } = req.query;
+    console.log(eventId, dayPlanId)
+
+    DayPlan.updateOne({_id:dayPlanId}, {$push: {events: eventId}})
+                    .then(() => {
+                        console.log('Event added to day plan');                      
+                    })
+                    .catch((err) => console.error(err));
 }
 );
 
