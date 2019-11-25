d1ae77ecb809348f2f671b36763398c41d8f9e81
nurlopez <nlopezcolom@gmail.com>
2019-11-25 19:29:30 +0100

    Create edit-profile view Create edit-profile routes Edit user profile fields OK Back to home OK Add edit-profile button in Home view

--- routes/edit-profile.js
+++ routes/edit-profile.js
@@ -11,8 +11,26 @@ const User = require('../models/User');
 router.get('/', (req, res, next) => {
     
     const currentUserData = req.session.currentUser
-               res.render('./edit-profile', {currentUserData}) ;
-   
+               res.render('./edit-profile', {currentUserData}) 
                 });
-          
+
+router.post('/', (req, res, next) => {
+
+    const userId = req.session.currentUser._id
+    
+    const {fullName, email, keywords} = req.body;
+        
+    mongoose
+    .connect(dbUrl + dbName, {useNewUrlParser: true, useUnifiedTopology:true})
+        .then( mongoEntry => {
+            
+            User.updateOne({_id:userId}, {$set:{'fullName':fullName, 'email': email, 'keywords': keywords}})
+                .then (() => { 
+                    
+                });    
+    });    
+
+})
+
+
 module.exports = router;
