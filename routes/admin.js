7c9254c2a8aba28eeb4238d6a97bd8a511a94f86
Johann <johanson88@gmail.com>
2019-11-25 01:13:20 +0100

    Admin update event OK

--- routes/admin.js
+++ routes/admin.js
@@ -19,19 +19,42 @@ const sessionList = [];
 router.get('/', (req, res, next) => {
     if (req.session.currentUser.admin) {
         mongoose.connect(dbUrl + dbName,{useNewUrlParser : true,
-            useUnifiedTopology: true})
-            
+            useUnifiedTopology: true})    
             .then( () => {
                 Event.find()
                     .then(events =>{
+                        mongoose.connection.close();
                         events.forEach(event =>eventsList.push(event));                         
                         res.render('./admin/admin', {eventsList});
                     })
             })
+            .catch(err => console.error(err));
         
     }else res.render('./error',{error:'NOT AUTHORIZED'});
      
    
+});
+
+router.post('/update/event', (req,res,next) => {
+    if (req.session.currentUser.admin) {
+        const {_id,eventName,fullAddress,location,date,startTime,endTime,price,category,coordinates,meetupLink} = req.body;
+        // Mongoose sends a `updateOne({ _id: doc._id }, { $set: { name: 'foo' } })`
+        console.log(_id);
+        mongoose.connect(dbUrl + dbName,{useNewUrlParser : true,
+            useUnifiedTopology: true})
+            .then( () => {
+                Event.updateOne({_id},{$set:{'eventName':eventName,'startTime':startTime,'price':price,endTime:endTime,'meetupLink':meetupLink,fullAddress:fullAddress,date:date,location:location,'category':category,'coordinates':coordinates}})
+                    .then(()=>{
+                        console.log('Updated');
+                        res.render('./admin/admin', {eventsList});
+                    })
+                    .catch(err => console.error(err));
+            })
+            .catch(err => console.error(err));
+        
+    }else res.render('./error',{error:'NOT AUTHORIZED'});
+    
+    
 });
 
 module.exports = router;
