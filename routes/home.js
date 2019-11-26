57c2afc377eeb91bc7ef40c1cc5bae2b6794cdc7
Johann <johanson88@gmail.com>
2019-11-26 12:39:27 +0100

    patch home.js

--- routes/home.js
+++ routes/home.js
@@ -5,17 +5,18 @@ const router = express.Router();
 const User = require('./../models/User');
 const DayPlan = require('./../models/Dayplan');
 
-const userId = req.session.currentUser._id;
 
 router.get('/', (req, res, next) => {
-        User.find({_id: userId})
-            .then ((userData) => {               
-                const userAgenda = userData[0].agenda;                                
-                    DayPlan.find({_id: {$in: userAgenda}})
-                    .populate('events')
-                    .then ( (dayPlanFound) => {
-                        res.render('./home', {dayPlanFound});             
-            })
+    const userId = req.session.currentUser._id;
+
+    User.find({_id: userId})
+        .then ((userData) => {               
+            const userAgenda = userData[0].agenda;                                
+                DayPlan.find({_id: {$in: userAgenda}})
+                .populate('events')
+                .then ( (dayPlanFound) => {
+                    res.render('./home', {dayPlanFound});             
+        })
     })
 
 })
