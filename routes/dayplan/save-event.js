58bb0e984772e2e1c3db4262e22c4966818eb8c6
Johann <johanson88@gmail.com>
2019-11-27 23:45:59 +0100

    Save place in DDBB

--- routes/dayplan/save-event.js
+++ routes/dayplan/save-event.js
@@ -1,18 +1,27 @@
 const express = require('express');
 const router = express.Router();
-
+const axios = require('axios');
 
 const DayPlan = require('./../../models/Dayplan');
 const Event = require('./../../models/Event');
 
 //esto hay que cambiarlo con un buen model
 router.post('/dine', (req,res,next) => {
-    console.log(req.body);
     const {dayPlanId,name,address,time} = req.body;
-    console.log(dayPlanId,name,address,time);
     Event.create({eventName:name,fullAddress:address,category:"food",startTime:time})
         .then( (event) =>{
             console.log(event);
+            // axios.post('/dayplan/save-event',{eventId:event._id, dayPlanId:dayPlanId})
+            //     .then(() => console.log('Done'))
+            //     .catch( (err) => console.log(err));
+            DayPlan.updateOne({_id:dayPlanId}, {$push: {events: event._id}})
+                    .then(() => {
+                        console.log('Event added to day plan');  
+                        res.status(200).send();
+                        res.end();             
+                    })
+                    .catch((err) => console.error(err));
+                
         })
         .catch((err) => console.error(err));
     
