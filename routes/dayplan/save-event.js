119f9f260c251f2789f7d889b701952529bc4815
Johann <johanson88@gmail.com>
2019-11-28 10:36:00 +0100

    Event add semiworking

--- routes/dayplan/save-event.js
+++ routes/dayplan/save-event.js
@@ -30,7 +30,7 @@ router.post('/dine', (req,res,next) => {
 })
 
 router.post('/', (req, res, next) => {
-    
+    console.log(req.body,'query')
     const {eventId, dayPlanId } = req.body;
     console.log('Add event query',eventId,dayPlanId);
     DayPlan.updateOne({_id:dayPlanId}, {$push: {events: eventId}})
